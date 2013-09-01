81839d41822cc3d84844f9f0f0a769d47ccfc8a8
junp <junpmail@163.com>
2013-09-01 23:51:48 +0800

    comments

--- src/login/login.js
+++ src/login/login.js
@@ -1,14 +1,31 @@
 define(function(require, exports){
-
+	
+	/**
+	判断是否ios设备
+	@function isIos
+	@return boolean
+	**/
 	function isIos(){
 		var ua = navigator.userAgent.toLowerCase()
 		return ua.indexOf('iPhone') >= 0 || ua.indexOf('iPad') >= 0 || ua.indexOf('iPod') >= 0
 	}
+	
+	/**
+	登录类，根据访问设备返回不同样式登录链接
 
+	@class Login
+	@constructor
+	 **/
 	function Login(){
 		this.construct(url, options)
 	}
 	Login.prototype = {
+		/**
+		 登录类构造函数
+		 @method construct
+		 @param url {string} 登录成功回跳链接
+		 @param options {object}
+		 **/
 		construct: function(url, options){
 			var self = this
 			if(typeof url == 'object'){
@@ -18,6 +35,12 @@ define(function(require, exports){
 				self.options = $.extend(Login.defaults, {s_url: url})
 			}
 		},
+		
+		/**
+		 返回登录链接
+		 @method getLink
+		 @return {String} 登录链接
+		 **/
 		genLink: function(){
 			
 			var temp = []
@@ -30,6 +53,7 @@ define(function(require, exports){
 					
 		}
 	}
+
 	Login.defaults = {
 		style: isIos() ? 8 : 9,
 		appid: 0,
@@ -54,7 +78,18 @@ define(function(require, exports){
 		hln_feedback: '',
 		hln_copyright: ''
 	}
-
+	
+	/**
+	 对外接口
+	 @public 
+	 @method loginLink
+	 @param url {string} 登录成功后回跳链接
+	 @param options {object}
+	 style {number} 登录界面样式，目前支持8：ios, 9:Android
+	 appid:业务id
+	 daid:
+	 s_url:
+	 **/
 	exports.loginLink = function(url, options){
 		new Login(url, options)
 	}
--- src/ta/ta.js
+++ src/ta/ta.js
@@ -1,4 +1,9 @@
 define(function(require, exports){
+	/**
+	 @function ta
+	 @param id {number} 统计ID
+	 @param charset {string} 编码
+	 */
 	function ta(id, charset){
 		charset || (charset = 'utf-8')
 		document.write('<script charset="'+charset+'" src="http://tajs.qq.com/stats?sId='+id+'"><\/script>')
