8f37961a29ac784a07339b03dcfac1df3cd0913a
junp <junpmail@163.com>
2013-09-03 17:09:05 +0800

    touchSlider

---
+++ src/touchSlider/touchSlider.js
@@ -0,0 +1,21 @@
+//https://github.com/pushiming/sea-module-marketing-slider/blob/master/lib/touchSlider.js
+/**
+* @class TouchSlider
+* @param {String} container »›∆˜
+* @param {Objects} options ≈‰÷√
+*/
+function TouchSlider(container, options){
+	if (!container){
+		return null
+	}
+
+	if (options){
+		options.container = container
+	}
+	else{
+		options = typeof container === 'string' ? { container: container} : container
+	}
+
+	
+}
+
